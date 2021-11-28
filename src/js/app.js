b96d9ea4acd7b843dedc3dcddbc1c6faefd1cbb9
Metylene <aymerickpdev@gmail.com>
2021-11-28 20:30:19 +0100

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+function initialize() {
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+
+    console.log("Heyo !");
+}
+initialize();
